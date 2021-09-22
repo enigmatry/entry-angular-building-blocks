ab5297552e1af8178eb08fd1ddf25732e74b8178
Nikola Dragicevic <nikola.dragicevic@nultien.rs>
2021-09-22 21:15:54 +0200

    https://jira.enigmatry.com/browse/ETL-328 ETL-328 Create NPM package for Stylelint rules Tweaked allowed at rules.

--- projects/stylelint-config/index.js
+++ projects/stylelint-config/index.js
@@ -12,7 +12,7 @@ module.exports = {
   ],
   rules: {
     "alpha-value-notation": "number",
-    "at-rule-allowed-list": [ "use", "for", "if", "else", "include", "extend", "return", "error", "each", "mixin", "function", "keyframes", "font-face" ],
+    "at-rule-allowed-list": [ "use", "for", "if", "else", "include", "extend", "return", "error", "each", "mixin", "function", "keyframes", "font-face", "forward", "at-root" ],
     "at-rule-name-case": "lower",
     "at-rule-name-space-after": "always",
     "at-rule-no-unknown": null,
