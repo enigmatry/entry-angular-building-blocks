02287a921fd5ed3fdae6906438ab48bff1e5715f
Marko Maric <mmr@enigmatry.com>
2021-09-29 12:47:11 +0000

    Merged PR 509: fix row-context-menu-formatter logic
    fix row-context-menu-formatter logic

--- projects/angular-building-blocks/enigmatry-grid/context-menu.component.ts
+++ projects/angular-building-blocks/enigmatry-grid/context-menu.component.ts
@@ -1,18 +1,29 @@
-import { Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation } from '@angular/core';
+import { RowContextMenuFormatter } from '.';
 import { ContextMenuItem } from './grid.interface';
 
 @Component({
-  selector: 'enigmatry-grid-context-menu',
-  templateUrl: './context-menu.component.html',
-  styleUrls: ['./context-menu.component.scss'],
-  encapsulation: ViewEncapsulation.None
+    selector: 'enigmatry-grid-context-menu',
+    templateUrl: './context-menu.component.html',
+    styleUrls: ['./context-menu.component.scss'],
+    encapsulation: ViewEncapsulation.None,
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class EnigmatryGridContextMenuComponent implements OnInit {
+    @Input() items: ContextMenuItem[] = [];
+    @Input() rowMenuFormatter: RowContextMenuFormatter;
+    @Input() rowData: any;
+    @Output() selected = new EventEmitter<string>();
 
-  @Input() items: ContextMenuItem[] = [];
-  @Output() selected = new EventEmitter<string>();
+    menuItems: ContextMenuItem[] = [];
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void { }
+    ngOnInit(): void {
+        if (this.rowMenuFormatter.items) {
+            this.menuItems = this.rowMenuFormatter.items(this.rowData);
+        } else {
+            this.menuItems = this.items;
+        }
+    }
 }
