a12117ade7e9636159c3eac51c608e246244771e
Biljana Miladinov <bma@enigmatry.com>
2021-10-21 11:11:16 +0000

    Merged PR 777: Qti inline choice interaction - styling is fixed
    Qti inline choice interaction - styling is fixed

--- projects/qti-rendering/src/lib/pipes/calculate-input-width.pipe.ts
+++ projects/qti-rendering/src/lib/pipes/calculate-input-width.pipe.ts
@@ -5,23 +5,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CalculateInputWidthPipe implements PipeTransform {
 
-  transform(value: string, defaultSize: number, maxSize: number, longestText: string): number {
-    let text = value;
-    if (!value) {
-      text = longestText;
-    }
+  transform(value: string, defaultMaxSize: number, maxSize: number, longestText: string): number {
     const dummy = document.createElement('div');
-    dummy.innerText = text;
+    dummy.innerText = (!value) ? longestText : value;
     dummy.style.position = 'absolute';
     dummy.style.visibility = 'hidden';
+    dummy.style.width = 'fit-content';
     document.body.insertBefore(dummy, document.body.firstChild);
-    const measuredWidth = dummy.clientWidth;
+    const measuredWidth = dummy.clientWidth + 12;
     document.body.removeChild(dummy);
-    if (!value) {
-      return Math.min(measuredWidth + 50, defaultSize);
-    } else {
-      return Math.min(measuredWidth + 50, maxSize);
-    }
+    return (!value) ? Math.min(measuredWidth, defaultMaxSize) :  Math.min(measuredWidth, maxSize);
   }
-
 }
