0636954dfdf809effb312a097685ca5c618f20c9
Miljan Šljivić <miljan.sljivic@novalite.rs>
2023-05-17 14:10:05 +0200

    Add PagedQuery and SearchFilterPagedQuery base model classes.

--- projects/entry-table/src/lib/interfaces/index.ts
+++ projects/entry-table/src/lib/interfaces/index.ts
@@ -1,6 +1,8 @@
 
 export * from './pagination';
 export * from './entry-table-config';
+export * from './paged-query';
+export * from './search-filter-paged-query';
 
 export * from './cell-template';
 export * from './column-def';
