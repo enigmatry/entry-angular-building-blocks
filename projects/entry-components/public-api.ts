9800f8db5c2942034d0bb4303c289497cb9f9092
Miljan Šljivić <miljan.sljivic@novalite.rs>
2023-05-22 13:05:48 +0200

    Remove src/lib sub-folders to reduce nesting and have cleaner import path.

---
+++ projects/entry-components/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of entry-components
+ */
+
+export * from '@enigmatry/entry-components/entry-header';
+export * from '@enigmatry/entry-components/entry-dialog';
--- projects/entry-components/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of entry-components
- */
-
-
-export * from '@enigmatry/entry-components/src/lib/entry-header';
-export * from '@enigmatry/entry-components/src/lib/entry-dialog';
