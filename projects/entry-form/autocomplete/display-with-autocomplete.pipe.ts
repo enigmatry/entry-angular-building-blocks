5f552224f104f215bb7fa1e82903a50e070207b4
msljivic <msl@enigmatry.com>
2023-05-23 08:47:45 +0200

    Create separate bundles for each form component. (#52)
    Component import path will be @enigmatry/entry-form/xxx

--- projects/entry-form/autocomplete/display-with-autocomplete.pipe.ts
+++ projects/entry-form/autocomplete/display-with-autocomplete.pipe.ts
@@ -1,5 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { SelectOption } from '../../../interfaces';
+import { SelectOption } from './select-configuration.interface';
 
 @Pipe({
   name: 'displayWithAutocomplete'
