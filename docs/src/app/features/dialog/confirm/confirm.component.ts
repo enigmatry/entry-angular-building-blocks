e331ca551f3db768b6c08a28fb439f320500227d
Marko Maric <marko.maric@novalite.rs>
2023-05-24 13:05:58 +0200

    add confirm implementation

--- docs/src/app/features/dialog/confirm/confirm.component.ts
+++ docs/src/app/features/dialog/confirm/confirm.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { EntryDialogService } from 'projects/entry-components/entry-dialog/entry-dialog.service';
 
 @Component({
   selector: 'app-confirm',
   templateUrl: './confirm.component.html',
   styleUrls: ['./confirm.component.scss']
 })
-export class ConfirmComponent implements OnInit {
+export class ConfirmComponent {
+  confirmResponse: boolean | undefined;
 
-  constructor() { }
+  constructor(private _entryDialog: EntryDialogService) { }
 
-  ngOnInit(): void {
-  }
-
+  openConfirm = () => this._entryDialog
+    .openConfirm({
+      title: `CONFIRM`,
+      message: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. `
+      + `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, `
+      + `when an unknown printer took a galley of type and scrambled it to make a type specimen book. `
+      + `It has survived not only five centuries, but also the leap into electronic typesetting, `
+      + `remaining essentially unchanged. It was popularized in the 1960s with the release of Letraset `
+      + `sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like `
+      + `Aldus PageMaker including versions of Lorem Ipsum.`
+    })
+    .subscribe((response: boolean | undefined) => this.confirmResponse = response);
 }
