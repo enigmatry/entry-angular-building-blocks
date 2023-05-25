497f720fd9da49590a1d48de6e01fc9271393d6d
mrle <marko.marich@gmail.com>
2023-05-25 21:04:25 +0200

    improve layout

--- docs/src/app/examples/dialog/alert/alert-example.component.ts
+++ docs/src/app/examples/dialog/alert/alert-example.component.ts
@@ -12,12 +12,6 @@ export class AlertExampleComponent {
 
   openAlert = () => this._entryDialog.openAlert({
     title: `ALERT`,
-    message: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. `
-    + `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, `
-    + `when an unknown printer took a galley of type and scrambled it to make a type specimen book. `
-    + `It has survived not only five centuries, but also the leap into electronic typesetting, `
-    + `remaining essentially unchanged. It was popularized in the 1960s with the release of Letraset `
-    + `sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like `
-    + `Aldus PageMaker including versions of Lorem Ipsum.`
+    message: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.`
   });
 }
