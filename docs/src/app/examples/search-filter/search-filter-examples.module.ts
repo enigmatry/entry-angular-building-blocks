4b122132d9608c97acc01669288e5ee964b556dd
Miljan Šljivić <miljan.sljivic@novalite.rs>
2023-05-29 11:48:42 +0200

    import from public-api

--- docs/src/app/examples/search-filter/search-filter-examples.module.ts
+++ docs/src/app/examples/search-filter/search-filter-examples.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchFilterExampleComponent } from './search-filter/search-filter-example.component';
 import { SharedModule } from '../../shared/shared.module';
-import { EntrySearchFilterModule } from 'projects/entry-components/search-filter/entry-search-filter.module';
-import { ENTRY_SEARCH_FILTER_CONFIG, EntrySearchFilterConfig } from 'projects/entry-components/search-filter/public-api';
+import {
+  ENTRY_SEARCH_FILTER_CONFIG, EntrySearchFilterConfig, EntrySearchFilterModule
+} from 'projects/entry-components/search-filter/public-api';
 import { MatTableModule } from '@angular/material/table';
 
 
