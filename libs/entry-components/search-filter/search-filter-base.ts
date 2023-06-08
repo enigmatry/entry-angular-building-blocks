c91d57ce5524b122fd9a674b34b863251841105d
Marko Maric <marko.maric@novalite.rs>
2023-06-08 09:10:10 +0200

    fix invalid comments and regenerate api docs; rename textbox to text-box

--- libs/entry-components/search-filter/search-filter-base.ts
+++ libs/entry-components/search-filter/search-filter-base.ts
@@ -8,14 +8,14 @@ export class SearchFilterBase<T> {
   key: string;
   /** Default value to be displayed/selected in the input control */
   value: T | undefined;
-  /** Default */
-  label: string;
   /** Label text to be displayed for the search-filter input control */
+  label: string;
+  /** Placeholder text for search-filter input control  */
   placeholder: string;
   /** Type of input control e.g. 'email' */
   type: string;
   order: number;
-  /** Control type to be overridden in implementing class, used to render the proper input type e.g. 'text box' */
+  /** Control type to be overridden in implementing class, used to render the proper input type e.g. 'text-box' */
   controlType: string;
   /** Max text length to be entered in the input component (default is 256) */
   maxLength: number;
--- libs/entry-components/search-filter/search-filter-textbox.ts
+++ libs/entry-components/search-filter/search-filter-textbox.ts
@@ -1,5 +1,5 @@
 import { SearchFilterBase } from './search-filter-base';
 
 export class SearchFilterInput extends SearchFilterBase<string> {
-  override controlType = 'textbox';
+  override controlType = 'text-box';
 }
