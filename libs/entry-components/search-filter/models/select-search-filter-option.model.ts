8b56d096abe2bf1e9aed16729898f6b0b9dd7b1d
Marko Maric <marko.maric@novalite.rs>
2023-06-12 15:42:14 +0200

    generate api docs

--- libs/entry-components/search-filter/models/select-search-filter-option.model.ts
+++ libs/entry-components/search-filter/models/select-search-filter-option.model.ts
@@ -1,3 +1,8 @@
+/** Model used to populate select filter options. */
 export class SelectSearchFilterOption {
-    constructor(public key: any | undefined, public label: string) {}
+    constructor(
+        /** Key used as a value for selected option */
+        public key: any | undefined,
+        /** String value used as display label of select option */
+        public label: string) {}
 }
