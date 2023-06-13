18cdb916539324c995b4853757fa49dd52aa648b
mrle <marko.marich@gmail.com>
2023-06-13 08:04:11 +0200

    remove order from filter

--- libs/entry-components/search-filter/search-filter-input/search-filter-base.model.ts
+++ libs/entry-components/search-filter/search-filter-input/search-filter-base.model.ts
@@ -14,7 +14,6 @@ export class SearchFilterBase<T> {
   placeholder: string;
   /** Type of input control e.g. 'email' */
   type: string;
-  order: number;
   /** Control type to be overridden in implementing class, used to render the proper input type e.g. 'text-input' */
   controlType: string;
   /** Max text length to be entered in the input component (default is 256) */
@@ -26,7 +25,6 @@ export class SearchFilterBase<T> {
     this.key = options.key || '';
     this.label = options.label || '';
     this.placeholder = options.placeholder || '';
-    this.order = options.order === undefined ? 1 : options.order;
     this.controlType = options.controlType || '';
     this.type = options.type || '';
     this.maxLength = options.maxLength || 256;
