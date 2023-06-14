089b9046a87db6997cd218f813c854c8e983aae1
Marko Maric <marko.maric@novalite.rs>
2023-06-14 11:09:54 +0200

    update copyServerSideValidationErrorsToForm

--- libs/entry-components/validation/entry-validation.ts
+++ libs/entry-components/validation/entry-validation.ts
@@ -1,30 +1,32 @@
-import { UntypedFormGroup } from '@angular/forms';
+import { UntypedFormGroup, ValidationErrors } from '@angular/forms';
 import { IValidationProblemDetails } from './validation-problem-details.interface';
 
 const DEFAULT_KEY = 'general';
+const FROM_SERVER_KEY = 'fromServer';
 
 const copyServerSideValidationErrorsToForm = (form: UntypedFormGroup, error: IValidationProblemDetails) => {
     form.setErrors(null);
     const validationErrors = error?.errors;
-    const formErrors: { [key: string]: string[] } = {};
-    const fileKeys: string[] = ['fileName', 'filePath', 'fileId', 'File'];
+    const formErrors: ValidationErrors = {};
 
     if (validationErrors) {
-        // eslint-disable-next-line guard-for-in
         for (const key in validationErrors) {
-            const  keySplitByCapital = key.split(/(?=[A-Z])/);
-
-            if (form.controls[key] && !fileKeys.some(fileKey => fileKey === key || keySplitByCapital.includes(fileKey))) {
+            if (form.controls[key]) {
                 const control = form.controls[key];
-                control.setErrors({ fromServer: validationErrors[key] });
+                const fieldErrors = {} as ValidationErrors;
+                fieldErrors[FROM_SERVER_KEY] = validationErrors[key];
+                control.setErrors(fieldErrors);
                 control.markAsTouched();
             } else {
-                const keyOrDefault = DEFAULT_KEY;
-                formErrors[keyOrDefault] = formErrors[keyOrDefault]?.concat(validationErrors[key]) || validationErrors[key];
-                form.setErrors(formErrors);
+                formErrors[DEFAULT_KEY] =
+                    formErrors[DEFAULT_KEY]?.concat(validationErrors[key]) || validationErrors[key];
             }
         }
+    } else {
+        formErrors[DEFAULT_KEY] = [ `An error occurred on the server.` ];
     }
+
+    form.setErrors(formErrors);
 };
 
 export {
