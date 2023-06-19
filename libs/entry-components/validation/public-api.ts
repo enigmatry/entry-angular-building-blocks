a7c7b416022030d6d6f64d363ea9d8fb915aff44
Marko Maric <marko.maric@novalite.rs>
2023-06-19 13:29:54 +0200

    added init entry-validation-configuration

--- libs/entry-components/validation/public-api.ts
+++ libs/entry-components/validation/public-api.ts
@@ -1,7 +1,9 @@
 export { EntryFormErrorsComponent } from './entry-form-errors.component';
 export { EntryFormFieldErrorsComponent } from './entry-form-field-errors.component';
 
+export { EntryValidationModule } from './entry-validation.module';
+
 export { IValidationProblemDetails } from './validation-problem-details.interface';
 export * from './entry-validation';
 
-export { EntryValidationModule } from './entry-validation.module';
+export { ENTRY_VALIDATION_CONFIG, EntryValidationConfig, IEntryValidationMessage } from './entry-validation-config.model';
