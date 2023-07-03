45dbb7c69885845f8919e4fc6b1b1ee7fd78e0ac
Miljan Šljivić <miljan.sljivic@novalite.rs>
2023-07-03 14:57:42 +0200

    Sanitize markdown html.

--- apps/demo-app/src/app/shared/markdown-viewer/markdown-viewer.component.ts
+++ apps/demo-app/src/app/shared/markdown-viewer/markdown-viewer.component.ts
@@ -1,5 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, SecurityContext } from '@angular/core';
 import { FileLoadService } from '../services/file-load.service';
+import * as md from 'markdown-it';
+import { map } from 'rxjs/operators';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-markdown-viewer',
@@ -7,19 +10,33 @@ import { FileLoadService } from '../services/file-load.service';
   styleUrls: ['./markdown-viewer.component.scss']
 })
 export class MarkdownViewerComponent implements OnInit {
-  @Input() filePath: string | undefined;
+  @Input() fileUrl: string | undefined;
   markdownContent: string | undefined;
 
-  constructor(private _fileLoad: FileLoadService) {}
+  constructor(
+    private _fileLoad: FileLoadService,
+    private _domSanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
-    if (this.filePath) {
-      this._fileLoad
-        .loadDocumentationFile(this.filePath)
-        .subscribe({
-          next: response => this.markdownContent = response,
-          error: _ => this.markdownContent = `### No API documentation found :'(`
-        });
+    if (this.fileUrl) {
+      this.loadFileContent();
     }
+  }
+
+  private loadFileContent() {
+    this._fileLoad
+      .loadDocumentationFile(this.fileUrl)
+      .pipe(
+        map(content => this.convertToHtml(content))
+      ).subscribe({
+        next: content => this.markdownContent = content,
+        error: _ => this.markdownContent = `### No API documentation found :'(`
+      });
+  }
+
+  private convertToHtml(markdown: string): string {
+    const markdownIt = md('default');
+    const html = markdownIt.render(markdown ?? '');
+    return this._domSanitizer.sanitize(SecurityContext.HTML, html);
   }
 }
