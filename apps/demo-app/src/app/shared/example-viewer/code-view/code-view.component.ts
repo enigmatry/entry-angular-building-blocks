b61dca0727412567680db641bbbf9bc43283e773
Miljan Šljivić <miljan.sljivic@novalite.rs>
2023-07-04 22:52:16 +0200

    code highlighting

--- apps/demo-app/src/app/shared/example-viewer/code-view/code-view.component.ts
+++ apps/demo-app/src/app/shared/example-viewer/code-view/code-view.component.ts
@@ -1,23 +1,40 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit, SecurityContext } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Clipboard } from '@angular/cdk/clipboard';
 import { FileExtension } from '../../models/file-extension.type';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import hljs from 'highlight.js';
 
 @Component({
   selector: 'app-code-view',
   templateUrl: './code-view.component.html',
-  styleUrls: ['./code-view.component.scss']
+  styleUrls: ['./code-view.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CodeViewComponent {
+export class CodeViewComponent implements OnInit {
   @Input() codeContent: string;
   @Input() codeType: FileExtension;
 
+  highlightedCode: SafeHtml;
+
   constructor(
     private _clipboard: Clipboard,
-    private _snackBar: MatSnackBar) {}
+    private _snackBar: MatSnackBar,
+    private _domSanitizer: DomSanitizer) { }
+
+  ngOnInit(): void {
+    this.highlightCode();
+  }
 
   copy = () => {
     this._snackBar.open(`Code copied to the clipboard!`);
     this._clipboard.copy(this.codeContent);
   };
+
+  private highlightCode() {
+    const highlightedCode = hljs.highlight(this.codeContent, { language: this.codeType, });
+    const sanitizedHtml = this._domSanitizer.sanitize(SecurityContext.HTML, highlightedCode.value);
+
+    this.highlightedCode = this._domSanitizer.bypassSecurityTrustHtml(sanitizedHtml);
+  }
 }
