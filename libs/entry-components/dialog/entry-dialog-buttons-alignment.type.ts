28f4c92022d4f58f33ee25317045f8f1c84f6014
Nikola Dragićević <nda@enigmatry.com>
2023-07-12 07:59:28 +0200

    Code now compiles again.

--- libs/entry-components/dialog/entry-dialog-buttons-alignment.type.ts
+++ libs/entry-components/dialog/entry-dialog-buttons-alignment.type.ts
@@ -1,2 +1,3 @@
 /** Defines horizontal alignment of dialog buttons. */
 export type EntryDialogButtonsAlignment = 'align-left' | 'align-center' | 'align-right';
+// TODO: sort out this by using classes from Scss Foundation instead of reinventing the wheel
