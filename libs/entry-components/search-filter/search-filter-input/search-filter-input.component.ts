9968d5eb07df7c0d8db5626e55f81551407ca95d
msljivic <msl@enigmatry.com>
2023-08-25 10:51:49 +0200

    Enable strict type checking in the templates (#140)
    * strictTemplates set to true

--- libs/entry-components/search-filter/search-filter-input/search-filter-input.component.ts
+++ libs/entry-components/search-filter/search-filter-input/search-filter-input.component.ts
@@ -3,6 +3,8 @@ import { UntypedFormGroup } from '@angular/forms';
 import { ControlType } from './control-type.model';
 import { ENTRY_SEARCH_FILTER_CONFIG, EntrySearchFilterConfig } from '../search-filter-config.model';
 import { SearchFilterBase } from './search-filter-base.model';
+import { SelectSearchFilter } from './inputs/select-search-filter.model';
+import { TextSearchFilter } from './inputs/text-search-filter.model';
 
 @Component({
   selector: 'entry-search-filter-input',
@@ -15,7 +17,12 @@ export class EntrySearchFilterInputComponent<T> {
   /** Form group to which the search-filter input component will be added. */
   @Input() form: UntypedFormGroup;
 
-  controlType = ControlType;
-
   constructor(@Inject(ENTRY_SEARCH_FILTER_CONFIG) public config: EntrySearchFilterConfig) { }
+
+  get textSearchFilter(): TextSearchFilter | undefined {
+    return this.searchFilter.controlType === ControlType.text && this.searchFilter as TextSearchFilter;
+  }
+  get selectSearchFilter(): SelectSearchFilter<T> | undefined {
+    return this.searchFilter.controlType === ControlType.select && this.searchFilter as SelectSearchFilter<T>;
+  }
 }
