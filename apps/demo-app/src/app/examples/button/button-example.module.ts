bcc7e50f9205746bb2db091538ea9c0349c9bdc8
Miljan Šljivić <miljan.sljivic@novalite.rs>
2023-08-29 12:14:22 +0200

    Show configuration options in button-example.module.ts

--- apps/demo-app/src/app/examples/button/button-example.module.ts
+++ apps/demo-app/src/app/examples/button/button-example.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonExampleComponent } from './button-example/button-example.component';
 import { SharedModule } from '../../shared/shared.module';
+import { ENTRY_BUTTON_CONFIG, EntryButtonConfig, EntryButtonModule } from '@enigmatry/entry-components/button';
 
 @NgModule({
   declarations: [
@@ -9,10 +10,18 @@ import { SharedModule } from '../../shared/shared.module';
   ],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    EntryButtonModule
   ],
   exports: [
     ButtonExampleComponent
-  ]
+  ],
+  providers: [{
+    provide: ENTRY_BUTTON_CONFIG,
+    useValue: new EntryButtonConfig({
+      submit: { type: 'raised', color: 'primary' },
+      cancel: { type: 'basic', color: 'accent' }
+    })
+  }]
 })
 export class ButtonExampleModule { }
