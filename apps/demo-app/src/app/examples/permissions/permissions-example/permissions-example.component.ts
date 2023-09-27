0a08c0f2f518aade9855a1ae37fff55d2a09f247
Miljan Šljivić <miljan.sljivic@novalite.rs>
2023-09-27 11:29:07 +0200

    Include module file in example.

--- apps/demo-app/src/app/examples/permissions/permissions-example/permissions-example.component.ts
+++ apps/demo-app/src/app/examples/permissions/permissions-example/permissions-example.component.ts
@@ -6,5 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./permissions-example.component.scss']
 })
 export class PermissionsExampleComponent {
-
 }
--- apps/demo-app/src/app/features/permissions/permissions.component.ts
+++ apps/demo-app/src/app/features/permissions/permissions.component.ts
@@ -11,5 +11,10 @@ export class PermissionsComponent {
     name: 'permissions.service.ts',
     path: 'permissions\\permissions-example\\permission.service',
     type: 'ts'
+  },
+  {
+    name: 'permissions-example.module.ts',
+    path: 'permissions\\permissions-example.module',
+    type: 'ts'
   }];
 }
