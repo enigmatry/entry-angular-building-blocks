08f719779eacf2ff82c363d80e116a37a5fcbb42
Miljan Šljivić <miljan.sljivic@novalite.rs>
2023-10-24 21:50:10 +0200

    Add form-on-submit.directive.ts

--- libs/entry-form/entry-form.module.ts
+++ libs/entry-form/entry-form.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { ENTRY_FIELD_TYPE_RESOLVER, fieldTypeResolverFactory, FieldTypeResolverService } from './services';
 import { FormlyAutocompleteModule } from './autocomplete/formly-autocomplete.module';
+import { FormOnSubmitDirective } from './directives/form-on-submit.directive';
 
 @NgModule({
   declarations: [
   ],
   imports: [
-    FormlyAutocompleteModule
+    FormlyAutocompleteModule,
+    FormOnSubmitDirective
+  ],
+  exports: [
+    FormOnSubmitDirective
   ],
   providers: [
     {
--- libs/entry-form/public-api.ts
+++ libs/entry-form/public-api.ts
@@ -5,3 +5,4 @@ export * from './interfaces';
 export * from './services';
 
 export { EntryFormModule } from './entry-form.module';
+export { FormOnSubmitDirective } from './directives/form-on-submit.directive';
