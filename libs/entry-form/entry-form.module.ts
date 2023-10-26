c23c5307009d552a667927e0f47d8b1462c0fdbd
Miljan Šljivić <miljan.sljivic@novalite.rs>
2023-10-26 23:17:49 +0200

    Remove previous solution.

--- libs/entry-form/entry-form.module.ts
+++ libs/entry-form/entry-form.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { ENTRY_FIELD_TYPE_RESOLVER, fieldTypeResolverFactory, FieldTypeResolverService } from './services';
 import { FormlyAutocompleteModule } from './autocomplete/formly-autocomplete.module';
-import { FormOnSubmitDirective } from './directives/form-on-submit.directive';
 
 @NgModule({
   declarations: [
   ],
   imports: [
     FormlyAutocompleteModule,
-    FormOnSubmitDirective
-  ],
-  exports: [
-    FormOnSubmitDirective
   ],
   providers: [
     {
