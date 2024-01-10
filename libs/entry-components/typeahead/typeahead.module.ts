2d689843f56cfb70597740d2252d5c98cbbb5b08
Miljan Šljivić <miljan.sljivic@novalite.rs>
2024-01-10 15:02:23 +0100

    Add basic mat autocomplete example for typeahead.

--- libs/entry-components/typeahead/typeahead.module.ts
+++ libs/entry-components/typeahead/typeahead.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EntryTypeaheadComponent } from './typeahead.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
     EntryTypeaheadComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
   ],
   exports: [
     EntryTypeaheadComponent
