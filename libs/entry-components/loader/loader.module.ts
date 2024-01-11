215c1600d76b3c77d39497b63a904d1344e77f04
Miljan Šljivić <miljan.sljivic@novalite.rs>
2024-01-11 15:10:07 +0100

    Remove loading.service.ts and loading.interceptor.ts from package.
    These will be added to blueprint template.

--- libs/entry-components/loader/loader.module.ts
+++ libs/entry-components/loader/loader.module.ts
@@ -1,25 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { OverlayModule } from '@angular/cdk/overlay';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { EntryLoaderComponent } from './entry-loader/loader.component';
-import { EntryLoadingBarComponent } from './entry-loading-bar/loading-bar.component';
 
 @NgModule({
   declarations: [
-    EntryLoaderComponent,
-    EntryLoadingBarComponent
+    EntryLoaderComponent
   ],
   imports: [
     CommonModule,
     OverlayModule,
-    MatProgressBarModule,
     MatProgressSpinnerModule
   ],
   exports: [
-    EntryLoaderComponent,
-    EntryLoadingBarComponent
+    EntryLoaderComponent
   ]
 })
 export class EntryLoaderModule { }
