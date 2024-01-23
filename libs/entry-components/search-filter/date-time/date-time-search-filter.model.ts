91ec1973a7060138c0eae8be610044e170bab1e1
Laslo Rajda <lra@enigmatry.com>
2024-01-23 09:50:07 +0100

    removed unnecessary override

--- libs/entry-components/search-filter/date-time/date-time-search-filter.model.ts
+++ libs/entry-components/search-filter/date-time/date-time-search-filter.model.ts
@@ -1,11 +1,6 @@
-import { FormControl } from '@angular/forms';
 import { ControlType } from '../control-type';
 import { SearchFilterBase } from '../search-filter-base.model';
 
 export class DateTimeSearchFilter extends SearchFilterBase<Date>{
     override controlType = ControlType.dateTime;
-
-    override toFormControl(): FormControl<Date> {
-      return new FormControl<Date>(this.value, {updateOn: 'change'});
-    }
 }
