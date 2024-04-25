2606c63b70993e372e07a1e9a1ae691822c925b5
Miljan Šljivić <miljan.sljivic@novalite.rs>
2024-04-25 17:02:47 +0200

    Remove ngx-mat-datetime-picker npm package.

--- libs/entry-form/date-time-picker/formly-date-time-picker.component.ts
+++ libs/entry-form/date-time-picker/formly-date-time-picker.component.ts
@@ -6,7 +6,6 @@ import { FieldType } from '@ngx-formly/material';
 @Component({
   selector: 'entry-formly-date-time-picker',
   templateUrl: './formly-date-time-picker.component.html',
-  styleUrls: ['./formly-date-time-picker.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FormlyDateTimePickerComponent extends FieldType<FormlyFieldConfig> {
--- libs/entry-form/date-time-picker/formly-date-time-picker.module.ts
+++ libs/entry-form/date-time-picker/formly-date-time-picker.module.ts
@@ -3,9 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormlyDateTimePickerComponent } from './formly-date-time-picker.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormlyModule } from '@ngx-formly/core';
-import { DATE_TIME_FORMATS } from './date-time-formats';
 import { EntryDateTimePickerModule } from '@enigmatry/entry-components/date-time-picker';
-import { MAT_DATE_FORMATS } from '@angular/material/core';
 
 @NgModule({
   declarations: [
@@ -21,12 +19,6 @@ import { MAT_DATE_FORMATS } from '@angular/material/core';
         { name: 'datetimepicker', component: FormlyDateTimePickerComponent }
       ]
     })
-  ],
-  providers: [
-    {
-      provide: MAT_DATE_FORMATS,
-      useValue: DATE_TIME_FORMATS
-    }
   ]
 })
 export class FormlyDateTimePickerModule { }
