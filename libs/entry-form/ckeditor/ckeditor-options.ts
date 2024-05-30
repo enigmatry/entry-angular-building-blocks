6fa3e43187458042bd34ed62e6ab4f11c57900c3
Miljan Šljivić <miljan.sljivic@novalite.rs>
2024-05-30 14:47:34 +0200

    Remove ckeditor

--- libs/entry-form/ckeditor/ckeditor-options.ts
+++
@@ -1,9 +0,0 @@
-import { InjectionToken } from '@angular/core';
-import { CKEditor5 } from '@ckeditor/ckeditor5-angular';
-
-export interface EntryCkeditorOptions {
-  build: CKEditor5.EditorConstructor;
-  config?: CKEditor5.Config;
-}
-
-export const ENTRY_CKEDITOR_OPTIONS = new InjectionToken<EntryCkeditorOptions>('EntryCkeditorOptions');
--- libs/entry-form/ckeditor/public-api.ts
+++
@@ -1,3 +0,0 @@
-
-export { FormlyCkeditorModule } from './formly-ckeditor.module';
-export * from './ckeditor-options';
