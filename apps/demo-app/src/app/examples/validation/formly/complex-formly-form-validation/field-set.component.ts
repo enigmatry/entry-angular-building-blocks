b08ac3c865c9c50c41bc8ab3913bb5936195d949
Nikola Dragićević <nda@enigmatry.com>
2026-04-06 23:54:34 +0200

    Migrated to control flow syntax.

--- apps/demo-app/src/app/examples/validation/formly/complex-formly-form-validation/field-set.component.ts
+++ apps/demo-app/src/app/examples/validation/formly/complex-formly-form-validation/field-set.component.ts
@@ -3,10 +3,14 @@ import { FieldType } from '@ngx-formly/core';
 
 @Component({
     selector: 'app-field-set',
-    template: `<fieldset *ngIf="field.fieldGroup as fieldGroup" class="validation-field-group">
-                <legend class="validation-group-legend">{{to.label}}</legend>
-                <formly-field *ngFor="let field of fieldGroup" [field]="field"></formly-field>
-            </fieldset>`,
+    template: `@if (field.fieldGroup; as fieldGroup) {
+                <fieldset class="validation-field-group">
+                    <legend class="validation-group-legend">{{props.label}}</legend>
+                    @for (field of fieldGroup; track field.name) {
+                        <formly-field [field]="field"></formly-field>
+                    }
+                </fieldset>
+            }`,
     standalone: false
 })
 export class FieldSetComponent extends FieldType {}
