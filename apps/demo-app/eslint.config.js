95bce5978cee4e4748e1962a3a48dca45205401b
Nikola Dragićević <nda@enigmatry.com>
2026-04-08 23:34:56 +0200

    Fixed linting.

--- apps/demo-app/eslint.config.js
+++ apps/demo-app/eslint.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
     ...defaultEnigmatryConfiguration,
-        {
+    {
         files: ['src/**/*.ts'],
         rules: {
             '@angular-eslint/prefer-standalone': 'off' // TODO: Remove when we get rid of Formly
--- apps/demo-app/src/app/app.component.ts
+++ apps/demo-app/src/app/app.component.ts
@@ -1,28 +1,28 @@
 import { Component, computed, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { filter } from 'rxjs/operators';
 import { IComponentDefinition, COMPONENT_DEFINITIONS } from './features/component-definitions';
 import { SortPipe } from './shared/pipes/sort.pipe';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    imports: [
-        RouterLink,
-        RouterLinkActive,
-        RouterOutlet,
-        MatToolbarModule,
-        MatSidenavModule,
-        MatListModule,
-        MatButtonModule,
-        SortPipe
-    ]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    RouterOutlet,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    SortPipe
+  ]
 })
 export class AppComponent {
   readonly menuItems = COMPONENT_DEFINITIONS;
@@ -34,7 +34,9 @@ export class AppComponent {
 
   readonly selectedMenuItem = computed((): IComponentDefinition | undefined => {
     const event = this.navigationEnd();
-    if (!event) { return undefined; }
+    if (!event) {
+      return undefined;
+    }
     return this.menuItems.find(item => event.url.includes(item.route));
   });
 }
